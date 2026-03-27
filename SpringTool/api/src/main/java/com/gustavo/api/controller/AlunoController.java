326f4e93edd5234c997b4271dccebc3adb99c241
Gustavo Pirondi <gustavopirondi@gmail.com>
2026-03-26 22:03:06 -0300

    Evolução API
    Finalização do CRUD de alunos

--- SpringTool/api/src/main/java/com/gustavo/api/controller/AlunoController.java
+++ SpringTool/api/src/main/java/com/gustavo/api/controller/AlunoController.java
@@ -2,32 +2,51 @@ package com.gustavo.api.controller;
 
 import com.gustavo.api.model.Aluno;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gustavo.api.repository.AlunoRepository;
+import com.gustavo.api.service.AlunoService;
 
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
 	
-	AlunoRepository repository;
+	private final AlunoService service;
 
-	public AlunoController(AlunoRepository repository) {
-		this.repository = repository;
+	public AlunoController(AlunoService service) {
+		this.service = service;
 	}
 	
 	@GetMapping
 	public List<Aluno> listar() {
-		return repository.findAll();
+		return service.listarTodos();
+	}
+	
+	@GetMapping("/{id}")
+	public Aluno buscarPorId(@PathVariable long id) {
+		return service.buscarPorId(id);
 	}
 	
 	@PostMapping
 	public Aluno salvar(@RequestBody Aluno aluno) {
-		return repository.save(aluno);
+		return service.salvar(aluno);
 	}
 	
+	@PutMapping("/{id}")
+	public Aluno atualizar(@PathVariable long id, @RequestBody Aluno aluno) {
+		return service.atualizar(id, aluno);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletar(@PathVariable long id) {
+		service.deletar(id);
+	}
 }
---
+++ SpringTool/api/src/main/java/com/gustavo/api/service/AlunoService.java
@@ -0,0 +1,40 @@
+package com.gustavo.api.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.gustavo.api.model.Aluno;
+import com.gustavo.api.repository.AlunoRepository;
+
+@Service
+public class AlunoService {
+	
+	private final AlunoRepository repository;
+	
+	public AlunoService(AlunoRepository repository) {
+		this.repository = repository;
+	}
+	
+	public List<Aluno> listarTodos(){
+		return repository.findAll();
+	}
+	
+	public Aluno buscarPorId(long id) {
+		return repository.findById(id)
+				.orElseThrow(()-> new RuntimeException("Aluno não encontrado"));
+	}
+	
+	public Aluno salvar(Aluno aluno) {
+		return repository.save(aluno); 
+	}
+	
+	public Aluno atualizar(long id, Aluno alunoAtt) {
+		Aluno aluno = buscarPorId(id);
+		aluno.setNome(alunoAtt.getNome());
+		aluno.setEmail(alunoAtt.getEmail());
+		return repository.save(aluno);
+	}
+	
+	public void deletar(long id) {
+		repository.deleteById(id);
+	}
+}
